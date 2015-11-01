0e7efeacd881f7dddc9a1abd831fb8074b60e2d5
einarr14 <einarr14@ru.is>
2015-11-01 21:27:15 +0000

    player klasinn kominn

--- src/main/java/is/sjosund/TicTacToeSjosund/Player.java
+++ src/main/java/is/sjosund/TicTacToeSjosund/Player.java
@@ -1,9 +1,49 @@
 package is.sjosund.TicTacToeSjosund;
 
-public class Player {
-    public static main(String[] args)
+public final class Point 
+{ 
+    private final int x;  
+    private final int y; 
+
+    public Point(int x, int y) 
     {
-        return 0;    
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Player {
+    private char c;
+    
+    public Player(char c)
+    {
+        this.c = c;
+    }
+    public static Point humanMove()
+    {
+        StdOut.printf("Enter row");
+        int x = StdIn.readInt();
+        StdOut.printf("Enter column");
+        int y = StdIn.readInt();
+        
+        Point p = new Point(x, y);
+        
+        return p;
+    }
+    
+    public static Point computerMove()
+    {
+        int random1 = Random.nextInt(3);
+        int random2 = Random.nextInt(3);
+        
+        Point P = new Point(random1, random2);
+        
+        return p;
+    }
+
+    public char playerChar()
+    {
+        return c;
     }
     
 }
--- src/test/java/is/sjosund/TicTacToeSjosund/PlayerTest.java
+++ src/test/java/is/sjosund/TicTacToeSjosund/PlayerTest.java
@@ -1,2 +1,12 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+public class PlayerTest
+{
+   @Test
+    public void testPlayerChar()
+    {
+        
+    }
+    
+}
