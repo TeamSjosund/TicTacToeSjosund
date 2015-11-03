9e6ce4c86ba4c0e68cfda74b2bbfe8f36762b899
einarr14 <einarr14@ru.is>
2015-11-03 18:11:35 +0000

    Sma lagfaeringar a deploy med pandoc

--- bin/deploy.bat
+++ bin/deploy.bat
@@ -2,6 +2,10 @@ set JAR="TicTacToeSjosund.jar"
 set TAR="TicTacToeSjosund.tar"
 set ZIP="TicTacToeSjosund.zip"
 set LOCATION="C:\temp"
+set DOCLOCATION="c:\temp\docs"
+set dr="designreport.html"
+set am="administrationmanual.html"
+set dm="developmentmanual.html"
 
 REM Clean, build and deploy
 call bin\clean.bat
@@ -12,9 +16,11 @@ call bin\unit_test.bat
 
 REM If directory exists, remove it
 if exist %LOCATION% del /F %LOCATION%
+if exist %DOCLOCATION% del /F %DOCLOCATION%
 
 REM If destionation folder doesn't exist, create it
 if not exist "%LOCATION%" mkdir %LOCATION%
+if not exist "%DOCLOCATION%" mkdir %DOCLOCATION%
 
 
 REM Copy the packaged files/report to directory outside working directory.
@@ -22,7 +28,9 @@ copy build\libs\%JAR% %LOCATION%
 copy build\distributions\%TAR% %LOCATION%
 copy build\distributions\%ZIP% %LOCATION%
 xcopy build\reports %LOCATION% /s /e /y
-xcopy docs\html %LOCATION$% /s /e /y
+copy docs\html\%am% %DOCLOCATION%
+copy docs\html\%dr% %DOCLOCATION%
+copy docs\html\%dm% %DOCLOCATION%
 
 REM Run application once
 java -jar %LOCATION%\\%JAR%
